5df45b72b6c4e729683daf8388fb251959e0a63b
sasaotomofumi <tomomasasso@gmail.com>
2020-10-30 08:26:41 +0900

    メッセージ送信機能の実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,4 @@
 class RoomUser < ApplicationRecord
- has_many :room_users 
- has_many :users, thriugh: :room_users 
- has_many :messages
-
- vakidates :name, presence: true 
+   belongs_to :room
+  belongs_to :user
 end
